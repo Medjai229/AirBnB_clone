57610ffd7e93dd3ce66afb31604d036c4dfce894
MMarzoo <mmarzouk12378@gmail.com>
2024-03-09 08:03:16 -0800

    updating file

--- models/base_model.py
+++ models/base_model.py
@@ -10,7 +10,7 @@ from uuid import uuid4
 
 class BaseModel:
     """Represents the BaseModel of the HBnB project."""
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         """Initialize a new BaseModel.
 
         Args:
@@ -20,6 +20,14 @@ class BaseModel:
         self.id = str(uuid4())
         self.created_at = datetime.now()
         self.updated_at = datatime.now()
+        if len(kwargs) != 0:
+            for key, value in kwargs.items():
+                if key == "__class__":
+                    continue
+                if key == "created_at" or key == "updated_at":
+                    self.__dict__[key] = datetime.fromisoformat(value)
+                else:
+                    self.__dict__[key] = value
 
     def save(self):
         """Update updated_at with the current datetime."""
